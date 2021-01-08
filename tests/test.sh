da6d897b42a345c5fac76efcd4171d2087d6671a
dmitrykersh <d.karpukhin@mail.ru>
2021-01-08 17:52:04 +0300

    Updated test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-curl -d "login user1" telnet://localhost:8080
+
+echo "This script opens 2 terminals for 2 server's clients. First you need to login: \"login <username>\". Commands: ping, clients. Timeout 20s"
+
+# to install xfce4-terminal:
+#
+# sudo apt update
+# sudo apt install xfce4-terminal
+
+xfce4-terminal -e "sh testuser1.sh" --window -e "sh testuser2.sh"
